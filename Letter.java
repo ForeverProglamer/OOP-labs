50395529b8a4e575cf1db76c9342a4b035140a88
ForeverProglamer <66169568+ForeverProglamer@users.noreply.github.com>
2020-06-03 16:14:39 +0300

    Add files via upload

---
+++ Letter.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class Letter {
+    private boolean isUpperCase;
+    private boolean isVowel;
+    private char symbol; // A, a
+
+    public Letter(char symbol) {
+        this.symbol = symbol;
+    }
+
+    @Override
+    public String toString() {
+        return "" + symbol;
+    }
+}
---
+++ Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Text text = new Text("LOL, are you really, do you like you it? Kek it`s barely nothing it`s. Chebyrek?");
+        System.out.println(text);
+        System.out.println(text.sentenceQuantityOfEntrances());
+
+    }
+}
